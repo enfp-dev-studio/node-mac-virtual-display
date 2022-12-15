6af64b0abaf0f1279ce974629b72a16659b83106
Jake Roh <enfpdev@gmail.com>
2022-12-15 11:06:25 +0900

    add create vdisplay

--- src/virtual_display.mm
+++ src/virtual_display.mm
@@ -87,7 +87,7 @@ public:
   //   static Napi::Object Init(Napi::Env env, Napi::Object exports);
   static Napi::Function GetClass(Napi::Env);
   VDisplay(const Napi::CallbackInfo &info);
-  Napi::Value GetDisplayId(const Napi::CallbackInfo &info);
+  //   Napi::Value GetDisplayId(const Napi::CallbackInfo &info);
   Napi::Value CreateVDisplay(const Napi::CallbackInfo &info);
 
 private:
@@ -98,17 +98,17 @@ private:
 };
 
 VDisplay::VDisplay(const Napi::CallbackInfo &info) : ObjectWrap(info) {
-  Napi::Env env = info.Env();
+  //   Napi::Env env = info.Env();
 }
 
 Napi::Function VDisplay::GetClass(Napi::Env env) {
-  return DefineClass(
-      env, "VDisplay",
-      {
-          VDisplay::InstanceMethod("getDisplayId", &VDisplay::GetDisplayId),
-          VDisplay::InstanceMethod("createVirtualDisplay",
-                                   &VDisplay::CreateVDisplay),
-      });
+  return DefineClass(env, "VDisplay",
+                     {
+                         //   VDisplay::InstanceMethod("getDisplayId",
+                         //   &VDisplay::GetDisplayId),
+                         VDisplay::InstanceMethod("createVirtualDisplay",
+                                                  &VDisplay::CreateVDisplay),
+                     });
 }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
@@ -118,40 +118,37 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
 }
 
 Napi::Value VDisplay::CreateVDisplay(const Napi::CallbackInfo &info) {
-  if (this->_display == nil) {
-
-    this->_descriptor = [[CGVirtualDisplayDescriptor alloc] init];
-    this->_descriptor.name = @"Virtual Display";
-    // this->_descriptor.name = "Test Display";
-    this->_descriptor.maxPixelsWide = 1920;
-    this->_descriptor.maxPixelsHigh = 1080;
-    this->_descriptor.sizeInMillimeters = CGSizeMake(1800, 1012.5);
-    this->_descriptor.productID = 0x1234;
-    this->_descriptor.vendorID = 0x3456;
-    this->_descriptor.serialNum = 0x0001;
-    this->_display =
-        [[CGVirtualDisplay alloc] initWithDescriptor:this->_descriptor];
-    this->_settings = [[CGVirtualDisplaySettings alloc] init];
-    this->_settings.hiDPI = 2;
-    this->_settings.modes = @[
-      [[CGVirtualDisplayMode alloc] initWithWidth:1920
-                                           height:1080
-                                      refreshRate:60],
-      [[CGVirtualDisplayMode alloc] initWithWidth:1920
-                                           height:1080
-                                      refreshRate:30],
-    ];
-    [this->_display applySettings:this->_settings];
-  }
+  this->_descriptor = [[CGVirtualDisplayDescriptor alloc] init];
+  this->_descriptor.name = @"Virtual Display";
+  // this->_descriptor.name = "Test Display";
+  this->_descriptor.maxPixelsWide = 1920;
+  this->_descriptor.maxPixelsHigh = 1080;
+  this->_descriptor.sizeInMillimeters = CGSizeMake(1800, 1012.5);
+  this->_descriptor.productID = 0x1234;
+  this->_descriptor.vendorID = 0x3456;
+  this->_descriptor.serialNum = 0x0001;
+  this->_display =
+      [[CGVirtualDisplay alloc] initWithDescriptor:this->_descriptor];
+  this->_settings = [[CGVirtualDisplaySettings alloc] init];
+  this->_settings.hiDPI = 2;
+  this->_settings.modes = @[
+    [[CGVirtualDisplayMode alloc] initWithWidth:1920
+                                         height:1080
+                                    refreshRate:60],
+    [[CGVirtualDisplayMode alloc] initWithWidth:1920
+                                         height:1080
+                                    refreshRate:30],
+  ];
+  [this->_display applySettings:this->_settings];
 
   Napi::Env env = info.Env();
   return Napi::Number::New(env, this->_display.displayID);
 }
 
-Napi::Value VDisplay::GetDisplayId(const Napi::CallbackInfo &info) {
-  Napi::Env env = info.Env();
-  return Napi::Number::New(env, this->_display.displayID);
-}
+// Napi::Value VDisplay::GetDisplayId(const Napi::CallbackInfo &info) {
+//   Napi::Env env = info.Env();
+//   return Napi::Number::New(env, this->_display.displayID);
+// }
 
 NS_ASSUME_NONNULL_END
 

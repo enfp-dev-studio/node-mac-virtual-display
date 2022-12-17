4501fd19fbc7a0b9ccc9536fc1377910a9895184
Jake Roh <enfpdev@gmail.com>
2022-12-17 23:49:01 +0900

    version up

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-// Type definitions for note-mac-virtual-dsiplay
-// Project: note-mac-virtual-dsiplay
+// Type definitions for node-mac-virtual-dsiplay
+// Project: node-mac-virtual-dsiplay
 
-export function getDisplayId(): number;
-export function createVirtualDisplay(): number;
+export function getDisplayId(): number
+export function createVirtualDisplay(): number
